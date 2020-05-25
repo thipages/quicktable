c3ed219940faf931cc372d678540b0fce60f279f
thipages <thipages@gmail.com>
2020-05-25 12:20:52 +0200

    new Qtag formalism implementation

--- src/thipages/quick/QTable.php
+++ src/thipages/quick/QTable.php
@@ -10,18 +10,13 @@ class QTable {
             if (!is_array($cell)) $cell=[$cell];
             $colNum=0;
             foreach ($cell as $col) {
-                $c[]=QTag::tag('td',$col);
+                $c[]=QTag::tag('td')($col);
                 $colNum++;
             }
-            $html_rows[]=QTag::tag('tr',join('',$c));
+            $html_rows[]=QTag::tag('tr')(join('',$c));
         }
-        if ($headers) foreach ($headers as $header) $html_header[]=QTag::tag('th',$header);
-        $this->html= QTag::tag(
-            'table',
-            join('',$html_header)
-            .join('',$html_rows),
-            $tableAttributes
-        );
+        if ($headers) foreach ($headers as $header) $html_header[]=QTag::tag('th')($header);
+        $this->html= QTag::tag('table',$tableAttributes)([$html_header,$html_rows]);
     }
     public function getHTML() {
         return $this->html;
